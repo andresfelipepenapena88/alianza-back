76d296ad6e5985708a36e54a85836ee13f63d7b7
Andres Peña <galletasinte@gmail.com>
2023-04-13 03:04:59 -0600

    init project

---
+++ ClientsManager/src/main/java/com/alianza/clientsmngr/clientsmanager/ClientsManagerApplication.java
@@ -0,0 +1,13 @@
+package com.alianza.clientsmngr.clientsmanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClientsManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClientsManagerApplication.class, args);
+	}
+
+}
